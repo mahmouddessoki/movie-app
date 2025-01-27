2bbb4d21bc511c79f38f5da7b4a6d9d9052f8f4c
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-01-28 01:09:53 +0200

    cccccc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,17 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
 import { MtDetailsComponent } from './components/mt-details/mt-details.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'movies', loadComponent: () => import("./components/movie/movie.component").then(c => c.MovieComponent) },
-  { path: 'tvshow', loadComponent: () => import("./components/tv-show/tv-show.component").then(c => c.TvShowComponent) },
-  { path: 'people', loadComponent: () => import("./components/people/people.component").then(c => c.PeopleComponent) },
-  {path: 'details/:media/:id' , component: MtDetailsComponent},
+  { path: 'home', component: HomeComponent , title: 'Home'  },
+  { path: 'movies', loadComponent: () => import("./components/movie/movie.component").then(c => c.MovieComponent) , title:"Movies" },
+  { path: 'tvshow', loadComponent: () => import("./components/tv-show/tv-show.component").then(c => c.TvShowComponent) , title:"TV Show"},
+  { path: 'people', loadComponent: () => import("./components/people/people.component").then(c => c.PeopleComponent) , title:"People" },
+  {
+    path: 'details/:media/:id', component: MtDetailsComponent,
+    data: {
+      prerender: true
+    },
+    title: 'Details'
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: "**", component: NotfoundComponent }
 
