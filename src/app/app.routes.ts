20874caf81a79166a46e1626177c0d5efb43a375
mahmouddessoki <mahmouddessoki33@gmail.com>
2025-01-28 01:13:04 +0200

    mmm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,13 +8,7 @@ export const routes: Routes = [
   { path: 'movies', loadComponent: () => import("./components/movie/movie.component").then(c => c.MovieComponent) , title:"Movies" },
   { path: 'tvshow', loadComponent: () => import("./components/tv-show/tv-show.component").then(c => c.TvShowComponent) , title:"TV Show"},
   { path: 'people', loadComponent: () => import("./components/people/people.component").then(c => c.PeopleComponent) , title:"People" },
-  {
-    path: 'details/:media/:id', component: MtDetailsComponent,
-    data: {
-      prerender: true
-    },
-    title: 'Details'
-  },
+  {path: 'details/:media/:id', component: MtDetailsComponent, title: 'Details'},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: "**", component: NotfoundComponent }
 
